4eef9a8ce3100c1adc248f9d9af20eceaa2a13c7
Gao Xin <gaoxin@dianlike.com>
2015-05-30 23:11:51 +0800

    Modified styles.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(function(){
   $('#fullpage').fullpage();
   $('.full-page').height = $(window).innerHeight();
+  $(window).on('orientationchange', function() {
+    location.reload();
+   });
 });
